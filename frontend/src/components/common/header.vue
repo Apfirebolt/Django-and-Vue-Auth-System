669efe2cd3a678243af19810835064897998caef
Amit P <aspper20@gmail.com>
2021-07-21 11:07:19 +0530

    login logout check applied

--- frontend/src/components/common/header.vue
+++ frontend/src/components/common/header.vue
@@ -11,15 +11,17 @@
       </div>
       <div class="hidden md:flex-1 md:flex md:items-center md:justify-around">
         <div class="flex items-center md:ml-12">
-          <router-link :to="{name: 'Login'}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-            Sign in
-          </router-link>
-          <router-link :to="{name: 'Register'}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-            Sign up
-          </router-link>
-          <a class="text-base font-medium text-gray-500 hover:text-gray-900 mx-4 cursor-pointer" @click.prevent="attemptLogout">
+          <a v-if="checkUserLoggedIn" class="text-base font-medium text-gray-500 hover:text-gray-900 mx-4 cursor-pointer" @click.prevent="attemptLogout">
             Log Out
           </a>
+          <div v-else>
+            <router-link :to="{name: 'Login'}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+              Sign in
+            </router-link>
+            <router-link :to="{name: 'Register'}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+              Sign up
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +30,15 @@
 <script>
 export default {
   name: 'HeaderComponent',
+  computed: {
+    checkUserLoggedIn() {
+      const storedToken = localStorage.getItem('access_token');
+      if (storedToken) {
+        return true;
+      }
+      return false;
+    },
+  },
   methods: {
     attemptLogout() {
       window.localStorage.removeItem('access_token');
